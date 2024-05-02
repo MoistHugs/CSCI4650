bbc2315a510be08a280c32b939ddba853ee8cd8a
MoistHugs <89755071+MoistHugs@users.noreply.github.com>
2024-05-01 23:36:29 -0500

    Update AlbumsDynamo.py

--- AlbumsDynamo.py
+++ AlbumsDynamo.py
@@ -3,9 +3,6 @@ import boto3
 # Initialize the DynamoDB client with the specified region
 dynamodb = boto3.client('dynamodb', region_name='us-east-1')  # Change 'us-east-1' to your desired region
 
-# Initialize the DynamoDB client
-dynamodb = boto3.client('dynamodb')
-
 # Define the table schema
 table_name = 'Albums'
 key_schema = [

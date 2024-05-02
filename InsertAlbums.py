f88c6e99f39b7d62d73ad6ed68e4ed1fccb86876
MoistHugs <89755071+MoistHugs@users.noreply.github.com>
2024-05-02 00:00:52 -0500

    Create InsertAlbums.py

---
+++ InsertAlbums.py
@@ -0,0 +1,37 @@
+import boto3
+
+# Initialize the DynamoDB client
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+
+# Define the table name
+table_name = 'Albums'
+
+# Define the items to be inserted
+items = [
+    {
+        'Artist': {'S': 'Pinkfong'},
+        'Songs': {'S': 'Baby Shark'},
+        'Album': {'S': 'Baby Shark Special'},
+        'ReleaseYear': {'N': '2016'}  # Example extra attribute
+    },
+    {
+        'Artist': {'S': 'Eminem'},
+        'Songs': {'S': 'Lose Yourself'},
+        'Album': {'S': '8 Mile Soundtrack'},
+        'ReleaseYear': {'N': '2002'}  # Example extra attribute
+    },
+    {
+        'Artist': {'S': 'Queen'},
+        'Songs': {'S': 'Bohemian Rhapsody'},
+        'Album': {'S': 'A Night at the Opera'},
+        'ReleaseYear': {'N': '1975'}  # Example extra attribute
+    }
+]
+
+# Insert items into the table
+for item in items:
+    response = dynamodb.put_item(
+        TableName=table_name,
+        Item=item
+    )
+    print("Item inserted successfully:", item)
